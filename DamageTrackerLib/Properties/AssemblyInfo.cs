c9fa90746ebc304736170a397ad1152e8942fcbc
Vari <tmarksinbox@gmail.com>
2023-01-19 00:33:53 +0000

    build: update assembly title

--- DamageTrackerLib/Properties/AssemblyInfo.cs
+++ DamageTrackerLib/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TestRPHPlugin")]
+[assembly: AssemblyTitle("DamageTrackerLib")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TestRPHPlugin")]
+[assembly: AssemblyProduct("DamageTrackerLib")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
